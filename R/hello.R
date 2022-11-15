d51ee755062272646e0dce9e2bb1b347a6ddd70a
Bruce Buurstra <buursbru@mail.gvsu.edu>
2022-11-15 18:44:00 -0500

    git

--- R/hello.R
+++ R/hello.R
@@ -16,3 +16,26 @@
 hello <- function() {
   print("Hello, world!")
 }
+
+library(readr)
+ratings_wide <- read_csv("ratings_wide.csv")
+View(ratings_wide)
+
+
+row_remover <- function(y) {
+rows <- unlist(rowSums(!is.na(ratings_wide))/9725)
+bad_rows <- c()
+for (x in 1:length(rows)) {
+  if (rows[x]<y){
+    print(x)
+    vector <- c(x)
+    bad_rows <- rbind(bad_rows, vector)
+    print("row added")
+  }
+  assign('bad_rows', bad_rows, envir = .GlobalEnv)
+}
+ratings_wide <- ratings_wide[-bad_rows,]
+assign('ratings_wide', ratings_wide, envir = .GlobalEnv)
+}
+
+row_remover(0.00226)
