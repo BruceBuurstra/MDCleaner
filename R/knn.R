e4f95cc0a6b871616cb761bcd4cccc92ee4ceed1
Bruce Buurstra <buursbru@mail.gvsu.edu>
2022-11-15 19:37:19 -0500

    merged functions

--- R/knn.R
+++ R/knn.R
@@ -10,7 +10,24 @@ drop_columns <- function() {
   assign('ratings_wide', ratings_wide, envir = .GlobalEnv)
 }
 
+row_remover <- function(y) {
+rows <- unlist(rowSums(!is.na(ratings_wide))/9725)
+bad_rows <- c()
+for (x in 1:length(rows)) {
+  if (rows[x]<y){
+    print(x)
+    vector <- c(x)
+    bad_rows <- rbind(bad_rows, vector)
+    print("row added")
+  }
+  assign('bad_rows', bad_rows, envir = .GlobalEnv)
+}
+ratings_wide <- ratings_wide[-bad_rows,]
+assign('ratings_wide', ratings_wide, envir = .GlobalEnv)
+}
+
 drop_columns()
+row_remover(0.00226)
 
 #view(drop_columns())
 print(nrow(ratings_wide))
