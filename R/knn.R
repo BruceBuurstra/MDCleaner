e0aef53d69b2ef9ccc27f417e4619d4809e17d2d
Bruce Buurstra <buursbru@mail.gvsu.edu>
2022-12-06 20:22:26 -0500

    updated function

--- R/knn.R
+++ R/knn.R
@@ -1,26 +1,26 @@
-library(readr)
-library(tidyverse)
-library(Matrix)
-library(factoextra)
-library(RcppML)
-library(pkgdown)
-library(usethis)
-library(magrittr)
-ratings_wide <- read_csv("ratings_wide.csv")
-ratings_wide <- ratings_wide %>% select(-`userId`)
+#library(readr)
+#library(tidyverse)
+#library(Matrix)
+#library(factoextra)
+#library(RcppML)
+#library(pkgdown)
+#library(usethis)
+#library(magrittr)
+#ratings_wide <- read_csv("MDCleaner/ratings_wide.csv")
+#ratings_wide <- ratings_wide %>% select(-`userId`)
 
-movie_titles <- read.csv("movies.csv")
+#movie_titles <- read.csv("MDCleaner/movies.csv")
 
 # Drop the columns having x or more percentage of missing values in dataset.
-drop_columns <- function(x) {
-  ratings_wide <- ratings_wide %>%
+drop_columns <- function(x, data) {
+  data <- data %>%
     purrr::discard(~sum(is.na(.x)) / length(.x) >= x)
-  assign("ratings_wide", ratings_wide, envir = .GlobalEnv)
+  return(data)
 }
 
 # Drop  the rows having 75% or more missing values in dataset
-drop_rows <- function(y) {
-  rows <- unlist(rowSums(!is.na(ratings_wide)) / ncol(ratings_wide))
+drop_rows <- function(y, data) {
+  rows <- unlist(rowSums(!is.na(data)) / ncol(data))
   bad_rows <- c()
   y <- 1 - y
   for (x in 1:length(rows)) {
@@ -28,16 +28,14 @@ drop_rows <- function(y) {
       vector <- c(x)
       bad_rows <- rbind(bad_rows, vector)
     }
-    assign("bad_rows", bad_rows, envir = .GlobalEnv)
   }
   if (length(bad_rows) > 0)
-    ratings_wide <- ratings_wide[-bad_rows, ]
-  assign("ratings_wide", ratings_wide, envir = .GlobalEnv)
+    data <- data[-bad_rows, ]
+  return(data)
 }
 
-
-drop_columns(.9)
-drop_rows(0.75)
+#columns_dropped <- drop_columns(0.9, ratings_wide)
+#rows_dropped <- drop_rows(0.75, ratings_wide)
 
 # Rounding the value to movie ratings
 round_to_movie_rating <- function(x) {
@@ -67,41 +65,41 @@ round_to_movie_rating <- function(x) {
 }
 
 # Reducing the dimension of dataset using nmf
-run_nmf_imputation <- function(z) {
-  ratings_wide_m <- ratings_wide
-  ratings_wide_m[is.na(ratings_wide_m)] <- 0
-  ratings_wide_m <- data.matrix(ratings_wide_m)
+run_nmf_imputation <- function(z, data) {
+  data_matrix <- data
+  data_matrix[is.na(data_matrix)] <- 0
+  data_matrix <- data.matrix(data_matrix)
 
-  nmf <- nmf(ratings_wide_m, z, mask = "zeros")
+  nmf <- nmf(data_matrix, z, mask = "zeros")
 
-  nmf_mult <- as.matrix(nmf@w %*% Diagonal(x = nmf@d) %*% nmf@h)
-  nmf_mult_round <- sapply(nmf_mult, round_to_movie_rating)
-  ratings_nmf_imputed <- matrix(unlist(nmf_mult_round),
-                                ncol = ncol(ratings_wide_m), byrow = FALSE)
-  assign("ratings_nmf_imputed", ratings_nmf_imputed, envir = .GlobalEnv)
+  data_nmf_imputed <- as.matrix(nmf@w %*% Diagonal(x = nmf@d) %*% nmf@h)
   assign("nmf", nmf, envir = .GlobalEnv)
+  return(data_nmf_imputed)
 }
 
-run_nmf_imputation(3)
+#nmf_imputed <- run_nmf_imputation(3, rows_dropped)
+#nmf_mult_round <- sapply(nmf_imputed, round_to_movie_rating)
+#nmf_mult_round <- matrix(unlist(nmf_mult_round),
+#                           ncol = ncol(ratings_wide_m), byrow = FALSE)
 
 # Finding the optimal number of kmeans cluster
-optimal_nmf_kmeans_clusters <- function() {
+optimal_nmf_kmeans_clusters <- function(nmf) {
   movies <- t(nmf@h)
   return(fviz_nbclust(movies, kmeans, method = "silhouette", k.max = 20))
 }
 
-optimal_nmf_kmeans_clusters()
+#optimal_nmf_kmeans_clusters(nmf)
 
 # Finding the elbow plot for kmeans clustering
-get_nmf_kmeans_elbow_plot <- function() {
+get_nmf_kmeans_elbow_plot <- function(nmf) {
   movies <- t(nmf@h)
   return(fviz_nbclust(movies, kmeans, nstart = 100, method = "wss"))
 }
 
-get_nmf_kmeans_elbow_plot()
+#get_nmf_kmeans_elbow_plot(nmf)
 
 # Clustering movies using nmf and kmeans
-run_nmf_movies_clustering <- function(w) {
+run_nmf_movies_clustering <- function(w, nmf) {
   movies <- t(nmf@h)
 
   kmean <- kmeans(movies, w, nstart = 100)
@@ -113,9 +111,9 @@ run_nmf_movies_clustering <- function(w) {
   return(plot1)
 }
 
-run_nmf_movies_clustering(3)
+#run_nmf_movies_clustering(3, nmf)
 
-get_nmf_clustered_data <- function() {
+get_nmf_clustered_data <- function(nmf, kmean) {
   movies <- t(nmf@h)
   kmeansClusters <- factor(kmean$cluster)
 
@@ -131,22 +129,22 @@ get_nmf_clustered_data <- function() {
   return(nmf_clustered_data)
 }
 
-nmf_clusterd_data <- get_nmf_clustered_data()
+#nmf_clusterd_data <- get_nmf_clustered_data(nmf, kmean)
 
-run_knn_imputation <- function(n) {
-  ratings_wide_m <- ratings_wide
-  ratings_wide_m[is.na(ratings_wide_m)] <- 0
-  ratings_wide_m <- data.matrix(ratings_wide_m)
-  euclidean_matrix <- matrix(nrow = ncol(ratings_wide_m),
-                             ncol = ncol(ratings_wide_m))
+run_knn_imputation <- function(n, data) {
+  data_matrix <- data
+  data_matrix[is.na(data_matrix)] <- 0
+  data_matrix <- data.matrix(data_matrix)
+  euclidean_matrix <- matrix(nrow = ncol(data_matrix),
+                             ncol = ncol(data_matrix))
 
-  for (i in 1:ncol(ratings_wide_m)) {
-    for (j in 1:ncol(ratings_wide_m)) {
-      i1 <- which(ratings_wide_m[, i] != 0)
-      i2 <- which(ratings_wide_m[, j] != 0)
+  for (i in 1:ncol(data_matrix)) {
+    for (j in 1:ncol(data_matrix)) {
+      i1 <- which(data_matrix[, i] != 0)
+      i2 <- which(data_matrix[, j] != 0)
       indx <- intersect(i1, i2)
-      int1 <- ratings_wide_m[indx, i]
-      int2 <- ratings_wide_m[indx, j]
+      int1 <- data_matrix[indx, i]
+      int2 <- data_matrix[indx, j]
       euc <- sqrt(sum(int1 - int2)^2)
       euclidean_matrix[i, j] <- euc
       print(i)
@@ -155,7 +153,7 @@ run_knn_imputation <- function(n) {
     assign("euclidean_matrix", euclidean_matrix, envir = .GlobalEnv)
   }
 
-  ratings_knn_imputed <- ratings_wide_m
+  knn_imputed <- data_matrix
   for (i in 1:ncol(ratings_knn_imputed)){
     dist <- euclidean_matrix[-i,i]
     distances <- sort(dist)[1:n]
@@ -163,47 +161,48 @@ run_knn_imputation <- function(n) {
     ret <-  list(neighbor_ind, distances)
     list <- c(unlist(ret[1]))
     print(i)
-    for (j in 1:nrow(ratings_knn_imputed)) {
-      if (ratings_knn_imputed[j, i] == 0) {
-        knn_values <- ratings_wide_m[j, list]
+    for (j in 1:nrow(knn_imputed)) {
+      if (knn_imputed[j, i] == 0) {
+        knn_values <- data_matrix[j, list]
         knn_non_zero <- which(knn_values != 0)
         knn_non_zero_values <- knn_values[knn_non_zero]
         if (length(knn_non_zero_values) == 0) {
-          ratings_knn_imputed[j, i] <- 0
+          knn_imputed[j, i] <- 0
         } else {
           avg_knn_values <- sum(knn_non_zero_values) /
             length(knn_non_zero_values)
-          ratings_knn_imputed[j, i] <- avg_knn_values
+          knn_imputed[j, i] <- avg_knn_values
         }
       } else {
         print(j)
       }
     }
   }
-  knn_round <- sapply(ratings_knn_imputed, round_to_movie_rating)
-  ratings_knn_imputed <- matrix(unlist(knn_round),
-                                ncol = ncol(ratings_wide_m), byrow = FALSE)
-  assign("ratings_knn_imputed", ratings_knn_imputed, envir = .GlobalEnv)
-  return(ratings_knn_imputed)
+  assign("knn_imputed", ratings_knn_imputed, envir = .GlobalEnv)
+  return(knn_imputed)
 }
 
-run_knn_imputation(5)
-optimal_knn_kmeans_clusters <- function() {
-  knn_movies <- t(ratings_knn_imputed)
+#run_knn_imputation(5, rows_dropped)
+#knn_round <- sapply(knn_imputed, round_to_movie_rating)
+#ratings_knn_imputed <- matrix(unlist(knn_round),
+ #                             ncol = ncol(ratings_wide_m), byrow = FALSE)
+
+optimal_knn_kmeans_clusters <- function(data) {
+  knn_movies <- t(data)
   return(fviz_nbclust(knn_movies, kmeans, method = "silhouette", k.max = 20))
 }
 
-optimal_knn_kmeans_clusters()
+#optimal_knn_kmeans_clusters(ratings_knn_imputed)
 
-get_knn_kmeans_elbow_plot <- function() {
+get_knn_kmeans_elbow_plot <- function(data) {
   knn_movies <- t(ratings_knn_imputed)
   return(fviz_nbclust(knn_movies, kmeans, nstart = 100, method = "wss"))
 }
 
-get_knn_kmeans_elbow_plot()
+#get_knn_kmeans_elbow_plot(ratings_knn_imputed)
 
-run_knn_movies_clustering <- function(v) {
-  knn_movies <- t(ratings_knn_imputed)
+run_knn_movies_clustering <- function(v, data) {
+  knn_movies <- t(data)
 
   knn_kmeans <- kmeans(knn_movies, v, nstart = 100)
 
@@ -214,11 +213,11 @@ run_knn_movies_clustering <- function(v) {
   return(plot1)
 }
 
-run_knn_movies_clustering(2)
+#run_knn_movies_clustering(2, ratings_knn_imputed)
 
-get_knn_clustered_data <- function(){
-  movies <- t(ratings_wide)
-  knn_movies <- t(ratings_knn_imputed)
+get_knn_clustered_data <- function(original, data){
+  movies <- t(original)
+  knn_movies <- t(data)
   knn_kmeansClusters <- factor(knn_kmeans$cluster)
 
   knn_movies_final <- as.data.frame(knn_movies) %>%
@@ -233,4 +232,5 @@ get_knn_clustered_data <- function(){
   return(knn_clustered_data)
 }
 
-knn_clustered_data <- get_knn_clustered_data()
+#knn_clustered_data <- get_knn_clustered_data(rows_dropped, ratings_knn_imputed)
+
